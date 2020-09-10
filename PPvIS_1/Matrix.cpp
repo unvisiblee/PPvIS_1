439f592f302a458b526f7e21b94e6838d445c575
Maksim Orlov <orlovmassimo@gmail.com>
2020-09-10 19:07:16 +0300

    f

--- PPvIS_1/Matrix.cpp
+++ PPvIS_1/Matrix.cpp
@@ -1,2 +1,38 @@
 #include "Matrix.h"
 #include <iostream>
+
+Matrix::Matrix()
+{
+}
+
+Matrix::~Matrix()
+{
+}
+
+void Matrix::setRowsNum(unsigned int)
+{
+}
+
+void Matrix::setColumnsNum(unsigned int)
+{
+}
+
+Matrix* Matrix::loadMatrixFromFile(const string&)
+{
+	return nullptr;
+}
+
+Matrix* Matrix::extractSubMatrix(unsigned int, unsigned int)
+{
+	return nullptr;
+}
+
+string Matrix::getMatrixType()
+{
+	return string();
+}
+
+Matrix* Matrix::transpose()
+{
+	return nullptr;
+}
